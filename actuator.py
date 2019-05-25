994644b6fe38888533b5036d7081dbf5bfe45ad8
beadurof <marcelo.temoteo@hotmail.com>
2019-05-25 14:22:58 -0300

    Actuator source code added

---
+++ actuator.py
@@ -0,0 +1,17 @@
+import numpy as np
+import socket
+
+class Actuator:
+    def _init_ (self, type, serialNumber):
+        self.type = type
+        self.serialNumber = serialNumber
+        if type > 3 and type < 8:
+            self.ID = type << 5
+            if serialNumber < 32 and serialNumber > 0:
+                self.ID += serialNumber
+            else:
+                raise AttributeException("ERROR: Invalid serial number. Must be inbetween 1 to 31")
+        else:
+            raise AttributeException("ERROR: Invalid type. Must be inbetween 4 to 7")
+
+    
