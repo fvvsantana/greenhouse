23a2148b46a7e9f14d791d16e49de947bd4fca90
beadurof <marcelo.temoteo@hotmail.com>
2019-05-25 19:16:57 -0300

    Connection function added

--- actuator.py
+++ actuator.py
@@ -21,15 +21,42 @@ Véi, foca no código
 
 class Actuator:
     def _init_ (self, type, serialNumber):
+        self.__sock = None
+        self.__port = 1337
+        self.__addr = '127.0.255.1'
+        self.__socket = socket.socket(socket.AF_NET, socket.SOCK_STREAM)
+
         self.type = type
         self.serialNumber = serialNumber
+
         if type > 3 and type < 8:
-            self.ID = type << 5
-            if serialNumber < 32 and serialNumber > 0:
-                self.ID += serialNumber
+            if serialNumber < 32 and serialNumber >= 0:
+                self.__ID = type << 5 | serialNumber
             else:
-                raise AttributeException("ERROR: Invalid serial number. Must be inbetween 1 to 31")
+                raise AttributeException("ERROR: Invalid serial number. Must be between 0 to 31")
         else:
-            raise AttributeException("ERROR: Invalid type. Must be inbetween 4 to 7")
+            raise AttributeException("ERROR: Invalid type. Must be between 4 to 7")
+    '''
+        A funcao 'connect' se conecta ao HUB na porta e ip padrao contidos atuador. Ele manda seu ID e espera uma resposta de 2 bytes contendo
+        a sua porta exclusiva para se conectar. Caso o pacote recebido nao seja de 2 bytes, ele fecha a comunicacao, manda um codigo
+        de erro e termina a execucao do atuador. Em caso de sucesso, o atuador se conecta na porta recebida e a comunicacao/execucao
+        do sistema continua
+    '''
+    def connect(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+            soc.bind((self.__addr, self.__port))
+            soc.connect((self.__addr, self.__port))
+            soc.send(bytes([self.ID]))
+            auxPort = soc.recv(2)
+            if len(auxPort) != 2:
+                soc.send(bytes([0]))
+                soc.close()
+                raise AttributeException("ERROR: Connection error")
+            else:
+                newPort = auxPort[0] << 8 | auxPort[1]
+                soc.send(bytes([255]))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.__addr,  newPort))
+        self.sock.connect((self.__addr, newPort))
 
     
