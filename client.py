df208b66fa2d28756fb14f60e7f8742042020fe2
Flavio Santana <fvvsantana@gmail.com>
2019-06-19 18:25:58 -0300

    Implement enum

--- client.py
+++ client.py
@@ -2,7 +2,16 @@ import struct
 import socket
 from enum import Enum
 
-#class DeviceCode(Enum):
+class ComponentCode(Enum):
+    client = 0
+    temperature_sensor = 1
+    humidity_sensor = 2
+    co2_sensor = 3
+    heater = 4
+    cooler = 5
+    irrigator = 6
+    co2_injector = 7
+
 
 class Client:
 
