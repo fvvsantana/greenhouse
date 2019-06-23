d911ae5c50306e10e94922e85e964e0ff467de14
lucasmatm <lucasmatm@gmail.com>
2019-06-23 17:41:09 -0300

    Comentei

--- sensor.py
+++ sensor.py
@@ -5,11 +5,11 @@ from math import pi, sin
 from time import sleep
 
 class Sensor:
-    def __init__(self, type, serialNumber):
+    def __init__(self, type, serialNumber): # Ao criar o objeto seta o basico das variaveis
         sensor_type = type
         self.type = type
         self.sock = None
-        if (sensor_type > 0 and sensor_type < 4):
+        if (sensor_type > 0 and sensor_type < 4):  # Verifica se os valores são validos
             if (serialNumber >= 0 and serialNumber < 32):
                 self.serial = type<<5 | serialNumber
             else:
@@ -18,10 +18,10 @@ class Sensor:
             raise AttributeException("ERROR: Invalid type number")
         
     
-    def handshake(self):
+    def handshake(self): # Realiza o handshake para montar a conexao
         new_port = 0
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-            soc.connect(('127.0.255.1',1337))
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc: # Começa uma conexao padrao para 
+            soc.connect(('127.0.255.1',1337))                           # pegar a porta da conexao definitiva
             soc.send(bytes([self.serial]))
             aux_port = soc.recv(2)
             if len(aux_port) != 2:
@@ -30,8 +30,9 @@ class Sensor:
                 new_port = aux_port[0]<<8 | aux_port[1]
             soc.send(bytes([1]))    
 
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect(('127.0.255.1',new_port))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Comeca a conexao definitiva
+        self.sock.connect(('127.0.255.1',new_port)) 
+        # Chama o loop de gerar os valores do sensor
         if (self.type == 1): #temp
                 threading.Thread(target = self.send_loop_temp, args = ()).start()
         elif (self.type == 2): # umid
@@ -39,26 +40,26 @@ class Sensor:
         elif (self.type == 3): # coo
                 threading.Thread(target = self.send_loop_co2, args=()).start()
     
-    def send(self, value):
+    def send(self, value): # Manda os valores pela conexao ja estabelecida
         bytes_to_send = struct.pack("f", value)
         self.sock.sendall(bytes_to_send)
         
 
-    def send_loop_co2(self):
+    def send_loop_co2(self): # Loop de gerar os valores do sensor de CO2
         inc = 0
         while (1):
             self.send((sin((inc*pi)/180.0)+1)*0.5)
             inc += 1
             sleep(1.0)
     
-    def send_loop_um(self):
+    def send_loop_um(self): # Loop de gerar os valores do sensor de umidade
         inc = 0
         while (1):
             self.send((sin((inc*pi)/180.0)+1) * 0.5)
             inc += 1
             sleep(1.0)
     
-    def send_loop_temp(self):
+    def send_loop_temp(self): # Loop de gerar os valores do sensor de temperatura
         inc = 0
         while (1):
             self.send(((sin((inc*pi)/180.0)+1) * 20)+5)
@@ -66,7 +67,7 @@ class Sensor:
             sleep(1.0)
         
         
-    def close(self):
+    def close(self): # Fecha o conexao
         self.sock.close()
 
 Sensor(1, 0)
